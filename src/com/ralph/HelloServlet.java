711d34d7d0c5dd9dd398a9dd4ce149d5b1edd4ed
guozhanxian <guozhanxian@gmail.com>
2017-08-09 10:39:41 +0800

    init

---
+++ src/com/ralph/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.ralph;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by guozhanxian on 2017/8/9.
+ */
+public class HelloServlet extends javax.servlet.http.HttpServlet
+{
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException
+    {
+        doGet(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><meata charset=\"utf-8\"></head><body><h1>Hello World</h1></body></html>");
+        out.flush();
+        out.close();
+    }
+}
